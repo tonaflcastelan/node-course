cb8fe4a4026c6de0daa3a5029425a098873eec14
tonacastelan <tonatiuh.flores@konfio.mx>
2020-01-10 12:58:22 -0600

    Refactorización de app.js

--- 03_bases_node/app.js
+++ 03_bases_node/app.js
@@ -1,26 +1,4 @@
-const argv = require('yargs')
-    .command('listar', 'Imprime en consola la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .command('crear', 'Genera un archivo con la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .help()
-    .argv;
+const argv = require('./config/yargs').argv;
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
---
+++ 03_bases_node/config/yargs.js
@@ -0,0 +1,19 @@
+const configs = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+};
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', configs)
+    .command('crear', 'Genera un archivo con la tabla de multiplicar', configs)
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
