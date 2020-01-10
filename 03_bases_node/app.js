bbf6e8e22dfb8fac6dad9d144e3ed8f30313e3f8
tonacastelan <tonatiuh.flores@konfio.mx>
2020-01-10 13:07:37 -0600

    se instala paquete colors

--- 03_bases_node/app.js
+++ 03_bases_node/app.js
@@ -1,4 +1,5 @@
 const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
@@ -11,7 +12,7 @@ switch(comando) {
         break;
     case 'crear':
         crearArchivo(argv.base, argv.limite)
-        .then(archivo => console.log(`Archivo creado: ${archivo}`))
+        .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
         .catch(err => console.log(err));
         break;
     default:
--- 03_bases_node/multiplicar/multiplicar.js
+++ 03_bases_node/multiplicar/multiplicar.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const colors = require('colors');
 
 let listarTabla = (base, limite = 10) => {
     return new Promise((resolve, reject) => {
@@ -10,7 +11,9 @@ let listarTabla = (base, limite = 10) => {
             reject(`El valor introducido ${base} no es un número`);
             return;
         }
-        let data = '';
+        console.log('==============='.green);
+        console.log(`Tabla de ${base}`.green);
+        console.log('==============='.green);
         for (let i = 1; i <= limite; i++) {
             console.log(`${base} * ${i} = ${base * i} \n`);
         }
