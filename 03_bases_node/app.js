7d849566059de7d35c5d0b961e1496e9a779d0c5
tonacastelan <tonatiuh.flores@konfio.mx>
2020-01-10 12:24:20 -0600

    Bases node - instalación paquete Yargs

--- 03_bases_node/app.js
+++ 03_bases_node/app.js
@@ -1,6 +1,21 @@
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+}).argv;
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = '1';
+console.log('limite', argv.limite);
+return false;
+// let argv = process.argv;
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
 
 crearArchivo(base)
 .then(archivo => console.log(`Archivo creado: ${archivo}`))
